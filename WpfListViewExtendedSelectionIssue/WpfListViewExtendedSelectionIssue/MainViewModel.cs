57eb06b2d1f378917db416e1cee1a8e2699f94fd
Sam Stevens <SStevens@daytonfreight.com>
2020-03-02 14:17:20 -0500

    committing test project

---
+++ WpfListViewExtendedSelectionIssue/WpfListViewExtendedSelectionIssue/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using GongSolutions.Wpf.DragDrop;
+using System.Collections.ObjectModel;
+
+namespace WpfListViewExtendedSelectionIssue
+{
+    public class MainViewModel : IDropTarget
+    {
+        public MainViewModel()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Items.Add($"item {i}");
+            }
+        }
+
+        public ObservableCollection<string> Items { get; set; } = new ObservableCollection<string>();
+
+        public void DragOver(IDropInfo dropInfo)
+        {
+
+        }
+
+        public void Drop(IDropInfo dropInfo)
+        {
+
+        }
+    }
+}
